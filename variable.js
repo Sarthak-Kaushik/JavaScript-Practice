45d4b7313c705cfcbc18cf0fa088a04eaacea248
sarthak <sarthak.kaushik99@gmail.com>
2023-12-07 23:16:10 +0530

    learning variables

---
+++ variable.js
@@ -0,0 +1,12 @@
+name = "Sarthak kaushik";
+console.log(name);
+age = 19;
+console.log(age);
+a = null;
+console.log(a);
+b = true;
+console.log(b);
+c = false;
+console.log(c);
+d = undefined;
+console.log(d);

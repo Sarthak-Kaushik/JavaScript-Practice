00a1d9be1924cf7a81fd70a472ab0514aa6a2dd8
sarthak <sarthak.kaushik99@gmail.com>
2023-12-11 20:01:28 +0530

    practice Questions 1 & 2

---
+++ Practice_Questions_1.js
@@ -0,0 +1,12 @@
+const product = {
+
+    productName: "Parker Jotter Standard CT Ball Pen",
+    rating: 4.2,
+    mrp: 285.0,
+    isDealOfTheDay: true,
+    discount: 5.0,
+    price: 270.0,
+
+};
+var finalPrice = null;
+console.log(product);
---
+++ Practice_Questions_2.js
@@ -0,0 +1,13 @@
+const profile  = {
+    fullName : "Sarthak Kaushik",
+    posts: 195,
+    followers: 569000,
+    following: 4,
+    profession: "Entrepreneur",
+    company: "Apna College | Ex--Microsoft, DRDO",
+    quote: "To educate someone is the highest privilage in the world",
+    isBlueTick: true,
+};
+
+console.log(profile);
+console.log(profile.isBlueTick);

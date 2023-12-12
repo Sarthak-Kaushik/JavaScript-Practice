5eb8e69d436c53f27aba1a1eb121c9c169771be9
sarthak <sarthak.kaushik99@gmail.com>
2023-12-12 07:44:15 +0530

    learning about operators

---
+++ operators.js
@@ -0,0 +1,58 @@
+// // Arithmetic Operators
+// let a = 10;
+// let b = 20;
+// console.log("a + b =",a + b);          // Addition
+// console.log("a - b =",a - b);          // Subtraction
+// console.log("a * b =",a * b);          // Multiplication
+// console.log("a / b =",a / b);          // Division
+// console.log("a % b =",a % b);          // Modulus || Remainder
+// console.log("a ** b =",a ** b);        // Exponentiation
+
+// // Unary Operators
+
+// let c = ++a;
+// console.log("c =",c);                  // Increment
+// let d = --b;                           
+// console.log("d =",d);                  // Decrement
+// console.log("a++ =",a++);              // Postfix Increment
+// console.log("b-- =",b--);              // Postfix Decrement
+
+// // Assignment Operators
+// let e = 10;                            
+// console.log("e =",e);                  // = Assignment
+// e += 10;
+// console.log("e += 10 =",e);            // += Addition Assignment
+// e -= 10;
+// console.log("e -= 10 =",e);            // -= Subtraction Assignment
+// e *= 10;
+// console.log("e *= 10 =",e);            // *= Multiplication Assignment
+// e /= 10;
+// console.log("e /= 10 =",e);            // /= Division Assignment
+// e %= 10;
+// console.log("e %= 10 =",e);            // %= Modulus Assignment
+// e **= 10;
+// console.log("e **= 10 =",e);           // **= Exponentiation Assignment
+
+
+// // Comparison Operators
+// let f = 10;
+// let g = 20;
+// console.log("f == g =",f == g);        // == Equal to
+// console.log("f != g =",f != g);        // != Not Equal to
+// console.log("f > g =",f > g);          // > Greater than
+// console.log("f < g =",f < g);          // < Less than
+// console.log("f >= g =",f >= g);        // >= Greater than or Equal to
+// console.log("f <= g =",f <= g);        // <= Less than or Equal to
+// console.log("f === g =",f === g);      // === Equal value and Equal type
+
+// Logical Operators
+let h = 30;
+let i = 20;
+let cond1 = h > i;
+let cond2 = h === 30;
+let cond3 = i < 20;
+console.log("cond1 && cond2 =",cond1 && cond2);      // && Logical AND
+console.log("cond1 || cond3 =",cond1 || cond3);      // || Logical OR
+console.log("cond1 || cond3 =",cond3 || cond3);      
+console.log("!cond1 =",!cond1);                      // ! Logical NOT
+

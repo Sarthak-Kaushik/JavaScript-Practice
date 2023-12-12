163700efec306038acfdab1d931e44aaf4658c6f
sarthak <sarthak.kaushik99@gmail.com>
2023-12-12 09:05:41 +0530

    learning about conditional statements

---
+++ conditions.js
@@ -0,0 +1,50 @@
+// conditional statements - if, else if, else
+
+// IF 
+
+let age = 10;
+// if(age > 18)
+// {
+//     console.log("You are eligible to vote");
+// }
+
+// if(age < 18)
+// {
+//     console.log("You are not eligible to vote");
+// }
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark") {
+//     color = "black";
+// }
+
+// if(mode === "light") {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// IF ELSE
+
+let mode = "dark";
+let color;
+
+if(mode === "dark") {
+    color = "black";
+} else {
+    color = "white";
+}
+
+console.log(color);
+
+// odd or even example
+
+let num = 11;
+if (num % 2 === 0) {
+    console.log("Number is EVEN");
+} else {
+    console.log("Number is ODD");
+}
+

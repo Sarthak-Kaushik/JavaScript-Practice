8ce198bf338016e51903cf9b6f3fc2771b73dd44
sarthak <sarthak.kaushik99@gmail.com>
2023-12-13 08:55:31 +0530

    learning loops and strings

---
+++ loops.js
@@ -0,0 +1,54 @@
+// Print 1  to 5
+
+// for(let i =1; i<=5; i++)
+// {
+//     alert("Sarthak Kaushik");
+// }
+
+//calculate ssum  of 1 to 5
+
+// let sum = 0;
+// for(let i = 1; i<=5; i++)
+// {
+//     sum +=i;
+// }
+// alert(sum);
+
+//calculate ssum  of 1 to n
+
+// let sum = 0;
+// let n = prompt("Enter a number");
+// for(let i = 1; i<=n; i++)
+// {
+//     sum +=i;
+// }
+// alert(sum);
+
+// Infinite loop
+
+// for(let i = 1; i<=5; i--)
+// {
+//     alert("Sarthak Kaushik");
+// }
+
+// wwhile loop
+// let i = 1;
+// let sum =0;
+// while(i<=5)
+// {
+//     sum += i;
+//     i++;
+// }
+// alert("The sum is " + sum + " and i is " + i + "");
+
+// do while loop
+
+// let i = 20;
+// let sum =0;
+// do
+// {
+//     sum += i;
+//     i++; 
+// }
+// while(i<=5)
+// alert("The sum is " + sum + " and i is " + i + "");

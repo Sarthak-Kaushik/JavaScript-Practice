480f29a487eda55074b7cd1d837a5ac7eae812e8
sarthak <sarthak.kaushik99@gmail.com>
2024-01-03 00:20:05 +0530

    functions and methods along with practice question 8

--- Functions.js
+++ Functions.js
@@ -1,17 +1,41 @@
-function myFunction(str)
-{
-    console.log("Hello " + str);
-    console.log("Welcome to Meerut my friend");
-    console.log("Meerut mein aapka swagat hai");
+// function myFunction(str)
+// {
+//     console.log("Hello " + str);
+//     console.log("Welcome to Meerut my friend");
+//     console.log("Meerut mein aapka swagat hai");
 
-}
+// }
 
-let num1 = prompt("Enter first number");
-if(num1 == 1)
-{
-    myFunction("Sarthak");
-}
-else 
-{
-    alert("You entered a wrong number");
-}
+// let num1 = prompt("Enter first number");
+// if(num1 == 1)
+// {
+//     myFunction("Sarthak");
+// }
+// else 
+// {
+//     alert("You entered a wrong number");
+// }
+
+
+// ARROW FUNCTIONS
+
+//sum function
+
+// function sum(a, b)
+// {
+//     return a+b;
+// }
+
+// multiply function
+
+// function multi(a, b)
+// {
+//     return a*b;
+// }
+
+const mul = (a, b) =>
+{   
+    return(a*b);
+};
+
+console.log(mul(2, 3));

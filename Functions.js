b97dfb3063504537b93eba5dd9384a6283bd66f1
sarthak <sarthak.kaushik99@gmail.com>
2024-01-02 23:55:54 +0530

    functions and methods along with practice question 8

---
+++ Functions.js
@@ -0,0 +1,17 @@
+function myFunction(str)
+{
+    console.log("Hello " + str);
+    console.log("Welcome to Meerut my friend");
+    console.log("Meerut mein aapka swagat hai");
+
+}
+
+let num1 = prompt("Enter first number");
+if(num1 == 1)
+{
+    myFunction("Sarthak");
+}
+else 
+{
+    alert("You entered a wrong number");
+}

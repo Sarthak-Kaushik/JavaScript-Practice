35272339654e2b9a6bf2a7de9012e83393b7e21c
sarthak <sarthak.kaushik99@gmail.com>
2024-01-05 23:48:37 +0530

    method and practice question 9

--- Method.js
+++ Method.js
@@ -1,6 +1,11 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-arr.forEach(function printVal(val)
+// arr.forEach(function printVal(val, ind, array)      // forEach is a method
+// {
+//     console.log(val, ind, array);                   // Used as a normal function
+// });
+
+arr.forEach((val) =>                                   // used as an arrow function
 {
     console.log(val);
 });

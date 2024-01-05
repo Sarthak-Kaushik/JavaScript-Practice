4adeabe82d5ceef5c5a576acb26c894c949552a5
sarthak <sarthak.kaushik99@gmail.com>
2024-01-05 08:23:28 +0530

    methods

---
+++ Method.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arr.forEach(function printVal(val)
+{
+    console.log(val);
+});
--- Practice_Questions_8.js
+++ Practice_Questions_8.js
@@ -3,25 +3,24 @@
 
 // Q1.b) Create an arrow function that does the same thing as above.
 
-function myFunction(str)
-{
-let j = 0;  
-for(let i = 0; i < str.length; i++)
-{
-    if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o'|| str[i] == 'u')
-    {
-       j++;
-    }
-    else if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O'|| str[i] == 'U')
-    {
-        j++;
-    }
-}
-    return j;
-}
-let abc = prompt("Enter a string: ").split("");
+// function myFunction(str)
+// {
+// let j = 0;  
+// for(let i = 0; i < str.length; i++)
+// {
+//     if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o'|| str[i] == 'u')
+//     {
+//        j++;
+//     }
+//     else if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O'|| str[i] == 'U')
+//     {
+//         j++;
+//     }
+// }
+//     return j;
+// }
 
-alert("Number of vowels in the string: " + myFunction(abc));
+// alert("Number of vowels in the string: " + myFunction(prompt("Enter a string: ")));
 
 // let myFunction = (str) =>
 // {
@@ -39,5 +38,4 @@ alert("Number of vowels in the string: " + myFunction(abc));
 //     }
 //        return j;
 // };
-// let abc = prompt("Enter a string: ").split("");
-// alert("The number of vowels in the given string is : " + myFunction(abc));
+// alert("The number of vowels in the given string is : " + myFunction(prompt("Enter a string: ")));

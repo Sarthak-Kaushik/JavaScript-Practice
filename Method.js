3a5265338f585b74b1efaf8a1f56d32dfd3a6d92
sarthak <sarthak.kaushik99@gmail.com>
2024-01-06 09:21:54 +0530

    Methods and their Practice Questions

--- Method.js
+++ Method.js
@@ -1,11 +1,65 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//forEach() METHOD. It runs the loop for each element in the array and takes a function as an argument.
 
 // arr.forEach(function printVal(val, ind, array)      // forEach is a method
 // {
 //     console.log(val, ind, array);                   // Used as a normal function
 // });
 
-arr.forEach((val) =>                                   // used as an arrow function
+// arr.forEach((val) =>                                   // used as an arrow function
+// {
+//     console.log(val*val);
+// });
+// console.log(arr);
+
+
+
+
+
+
+
+// MAP() METHOD. It runs the loop for each element in the array and returns a new array with the values 
+//returned by the function.
+
+// let newArr = arr.map((val) =>
+// {
+//     return val*val;
+// })
+// console.log(newArr);
+
+
+
+
+
+
+
+// FILTER() METHOD. It runs the loop for each element in the array and returns a new array with the 
+//filtered/conditioned values returned by the function.
+
+// let evenArr = arr.filter((val, ind, arr) =>
+// {
+//     return val%2===0;
+// })
+// console.log(evenArr);
+
+
+
+
+
+// REDUCE() METHOD. It runs the loop for each element in the array. Performs some operations and reduces the
+// array to a single value. It returns the single value.
+
+// let sum = arr.reduce((acc, curVal) => 
+// {
+//     return acc + curVal;
+// });
+// console.log(sum);   
+
+// to find the biggest number in the given array.
+let num = [1, 10, 5, 8, 9, 6, 7];
+let max = num.reduce((res, cur) =>
 {
-    console.log(val);
+    return res > cur ? res : cur;
 });
+console.log(max);
--- Practice_Questions_9.js
+++ Practice_Questions_9.js
@@ -1,1 +1,48 @@
-//Q1) For a given array of numbers, print the square of each value  using forEach() method.
+//Q1) For a given array of numbers, print the square of each value using forEach() method.
+
+// let arr = prompt("Enter a list of numbers seperated by commas:").split("");
+// arr.forEach((val, ind) => 
+// {
+//     console.log(`Square of ${val} at the index ${ind} is ${val*val}`);
+// });
+
+
+
+
+//Q2) WWe are given array of marks of students. Filter out marks of the students that scored 90.
+
+// let marks = prompt("Enter the marks of students seperated by commas: ").split(",");
+// let topper = marks.filter((val) =>
+// {
+//     return val>=90;
+// })
+// console.log(`the marks of the students more than 90 are ${topper}`);
+
+
+
+
+
+//Q3) Take a number n as input from the user. Create an array of numbers fromm 1 to n.
+// Use the reduce method to calculate sum of all numbers in the array.
+// Use the reduce method to calculate product of all numbers in the array.
+
+
+
+let n = prompt("Enter a number to find Sum and Product of all till that number: ");
+let arr = [];
+for(let i =1; i <= n; i++)
+{
+    arr.push(i);
+}
+
+let sum = arr.reduce((res, cur)=>
+{
+    return res + cur;
+});
+console.log(`The sum of all numbers till ${n} is ${sum}`);
+
+let product = arr.reduce((res, cur) =>
+{
+    return res * cur;
+});
+console.log(`The product off all numbers till ${n} is ${product}`);

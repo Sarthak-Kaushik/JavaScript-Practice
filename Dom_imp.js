e046ed5000265ce110f82f2cb5ed1d2f92e3941d
sarthak <sarthak.kaushik99@gmail.com>
2024-01-08 10:24:16 +0530

    DOM Manipulation and practice question relatd to it  part 1

--- Dom_imp.js
+++ Dom_imp.js
@@ -8,5 +8,40 @@
 // console.dir(parahs);
 
 
-let elements = document.querySelectorAll("p");
-console.dir(elements);
+// let elements = document.querySelectorAll("p");
+// console.dir(elements);
+
+// let elements2 = document.querySelectorAll(".heading");
+// console.dir(elements2);
+
+// let elements3 = document.querySelector("body");
+// console.dir(elements3);
+
+
+// PROPERTIES OF DOM MANIPULATION
+
+
+// 1. tagName
+// console.log(elements3.tagName);
+
+
+// 2. innerText 
+
+// console.log(elements3.innerText);
+
+
+
+// let div = document.querySelector("div");
+// console.dir(div);
+// console.log(div.innerText);
+
+
+// 3. innerHTML
+
+
+let div = document.querySelector("div");
+console.dir(div.innerHTML);
+
+let elements4 = document.querySelector("h3");
+console.dir(elements4);
+elements4.innerHTML = "<i>I am a new heading</i>";

a087a85761246d763109802018fad58d076b0ca6
sarthak <sarthak.kaushik99@gmail.com>
2024-02-03 00:23:32 +0530

    EVENTS 7

--- Practice_Questions12.js
+++ Practice_Questions12.js
@@ -1,1 +1,22 @@
-let button1 = document.querySelector("#b")
+//Q1 Create a toggle button taha changes the screen  to dark - mode when clicked and light - mode when clicked again.
+let curMode = document.querySelector("#mode");
+let body = document.querySelector("body");
+let mode = "light";
+curMode.addEventListener("click", () => {
+    if(mode === "light")
+    {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        curMode.innerHTML = "Set Dark Mode";
+        mode = "dark";
+    }
+    else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        curMode.innerHTML = "Set Light Mode";
+        mode = "light";
+    }
+
+
+    console.dir(mode);
+});

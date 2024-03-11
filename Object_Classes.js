ea9bef946941de02fcd6d7380fc0527517c59533
sarthak <sarthak.kaushik99@gmail.com>
2024-03-11 11:00:33 +0530

    Object and classes and this element

---
+++ Object_Classes.js
@@ -0,0 +1,12 @@
+const employee = {
+    calcTax() {                                       // This is a way to define a function in an object
+        console.log("Tax rate is 10%");
+    },
+    valcTax2 : function() {                           // This is also a way to define a function in an object
+        console.log("Tax rate is 10%");
+    }
+};
+
+const karanArjun = {
+    salary: 50000,
+}

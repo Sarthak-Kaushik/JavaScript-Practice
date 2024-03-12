f5130a9039bbc10bfafeebb6bed8f6ab489d1ed5
sarthak <sarthak.kaushik99@gmail.com>
2024-03-12 10:34:51 +0530

    Learning about class and constructor and how to use them

--- Object_Classes.js
+++ Object_Classes.js
@@ -1,12 +1,45 @@
-const employee = {
-    calcTax() {                                       // This is a way to define a function in an object
-        console.log("Tax rate is 10%");
-    },
-    valcTax2 : function() {                           // This is also a way to define a function in an object
-        console.log("Tax rate is 10%");
-    }
-};
+//       OBJEECTS
 
-const karanArjun = {
-    salary: 50000,
-}
+
+// const employee = {
+//     calcTax() {                                       // This is a way to define a function in an object
+//         console.log("Tax rate is 10%");
+//     },
+//     valcTax2 : function() {                           // This is also a way to define a function in an object
+//         console.log("Tax rate is 10%");
+//     }
+// };
+
+// const karanArjun = {
+//     salary: 50000,
+// };
+
+// karanArjun.__proto__ = employee;
+
+
+//        CLASS
+
+
+class ToyotaCar {
+    constructor(brand, mileage) {
+        console.log("Creating new object");
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+
+    start() {
+        console.log("Car Started");
+    }
+    stop() {
+        console.log("Car Stopped");
+    }
+    // setBrand(brand) {
+    //     this.brand = brand;      // Here, "this" keyword is used to refer to the current object. 
+    }                            // It is used to access the current object's properties and methods.
+
+let fortuner = new ToyotaCar("Fortuner", 10);
+// fortuner.setBrand("Fortuner");  // Here, fortuner is the current object and setBrand is the method of the current object.
+console.log(fortuner);
+let lexus = new ToyotaCar("Lexus", 12);    
+// lexus.setBrand("Lexus");        // Here, lexus is the current object and setBrand is the method of the current object.
+console.log(lexus);

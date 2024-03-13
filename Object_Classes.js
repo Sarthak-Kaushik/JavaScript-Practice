70dbc3dcf289be1106a70f17fc85f98af211352d
sarthak <sarthak.kaushik99@gmail.com>
2024-03-13 07:10:40 +0530

    Learning Inheritance

--- Object_Classes.js
+++ Object_Classes.js
@@ -20,26 +20,78 @@
 //        CLASS
 
 
-class ToyotaCar {
-    constructor(brand, mileage) {
-        console.log("Creating new object");
-        this.brand = brand;
-        this.mileage = mileage;
+// class ToyotaCar {
+//     constructor(brand, mileage) {
+//         console.log("Creating new object");
+//         this.brand = brand;
+//         this.mileage = mileage;
+//     }
+
+//     start() {
+//         console.log("Car Started");
+//     }
+//     stop() {
+//         console.log("Car Stopped");
+//     }
+//     // setBrand(brand) {
+//     //     this.brand = brand;      // Here, "this" keyword is used to refer to the current object. 
+//     }                            // It is used to access the current object's properties and methods.
+
+// let fortuner = new ToyotaCar("Fortuner", 10);
+// // fortuner.setBrand("Fortuner");  // Here, fortuner is the current object and setBrand is the method of the current object.
+// console.log(fortuner);
+// let lexus = new ToyotaCar("Lexus", 12);    
+// // lexus.setBrand("Lexus");        // Here, lexus is the current object and setBrand is the method of the current object.
+// console.log(lexus);
+
+
+
+// INHERITANCE
+
+
+// class parent {
+//     hello() {
+//         console.log("Hello from parent class");
+//     }
+// }
+
+// class child extends parent {
+// }
+
+// let obj = new child();
+// obj.hello();
+
+
+class Person {
+    eat() {
+        console.log("Eat");
     }
 
-    start() {
-        console.log("Car Started");
+    sleep() {
+        console.log("Sleep");
     }
-    stop() {
-        console.log("Car Stopped");
-    }
-    // setBrand(brand) {
-    //     this.brand = brand;      // Here, "this" keyword is used to refer to the current object. 
-    }                            // It is used to access the current object's properties and methods.
 
-let fortuner = new ToyotaCar("Fortuner", 10);
-// fortuner.setBrand("Fortuner");  // Here, fortuner is the current object and setBrand is the method of the current object.
-console.log(fortuner);
-let lexus = new ToyotaCar("Lexus", 12);    
-// lexus.setBrand("Lexus");        // Here, lexus is the current object and setBrand is the method of the current object.
-console.log(lexus);
+    work() {
+        console.log("do nothing");
+    }
+}
+class Engineer extends Person{
+    work() {
+        console.log("solve problem, build something");
+    }
+}
+let sarthak = new Engineer();
+sarthak.eat();
+sarthak.sleep();
+sarthak.work();
+
+class Doctor extends Person{
+    work() {
+        console.log("Treat patients");
+    }
+}
+
+let kanika = new Doctor();
+kanika.eat();
+kanika.sleep();
+kanika.work();

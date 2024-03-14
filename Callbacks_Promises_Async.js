d685c788fba08c9c3c955a37bced6b2a273f6ef7
sarthak <sarthak.kaushik99@gmail.com>
2024-03-14 23:56:32 +0530

    Callbacks, Promises and async part 2

--- Callbacks_Promises_Async.js
+++ Callbacks_Promises_Async.js
@@ -1,13 +1,32 @@
-// function hello() {
-//     console.log('Hello');
+ // function hello() {
+ //     console.log('Hello');
+ // }
+ // setTimeout(hello, 4000);
+
+
+// console.log("One");
+// console.log("Two");
+// setTimeout(() => {
+//     console.log("Hello");
+// }, 4000);    // Timeout is in milliseconds;
+// console.log("Three");
+// console.log("Four");
+
+
+// function sum(a, b) {
+//     console.log(a+b);
 // }
-// setTimeout(hello, 4000);
 
+// function calculator(a, b, sumCallback) {
+//     sumCallback(a, b);
+// }
 
-console.log("One");
-console.log("Two");
-setTimeout(() => {
-    console.log("Hello");
-}, 4000);    // Timeout is in milliseconds;
-console.log("Three");
-console.log("Four");
+// calculator(10, 5, sum);      // 15
+
+const hello = () => {
+    console.log("Hello World");
+}
+
+setTimeout(hello, 3000);
+
+console.log("Kaise hai aap sab");

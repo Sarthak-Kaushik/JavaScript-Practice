435a4628b3c15e451107919d9787223febc132d9
sarthak <sarthak.kaushik99@gmail.com>
2024-03-14 10:34:00 +0530

    Callbacks, Promises & Async part 1

---
+++ Callbacks_Promises_Async.js
@@ -0,0 +1,13 @@
+// function hello() {
+//     console.log('Hello');
+// }
+// setTimeout(hello, 4000);
+
+
+console.log("One");
+console.log("Two");
+setTimeout(() => {
+    console.log("Hello");
+}, 4000);    // Timeout is in milliseconds;
+console.log("Three");
+console.log("Four");

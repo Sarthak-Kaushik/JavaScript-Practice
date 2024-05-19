c400f191d9867559c74cf4968725122aa12ce6a7
sarthak <sarthak.kaushik99@gmail.com>
2024-05-20 02:38:35 +0530

    Callbacks Part 1

---
+++ Callbacks_Promises_Async.js
@@ -0,0 +1,14 @@
+console.log("Hello everyone!");
+function hello()
+{
+    // console.log("Hello I am Sarthak");
+}
+
+setTimeout(() => {
+    console.log("Hello I am Sarthak!");    // Timeout
+}, 3000);
+
+console.log("Hello everyone again!");
+
+// This is called Asyncronous programming. The code is not executed in the order it is written.
+// The code is executed in the order of the time it takes to execute the code.

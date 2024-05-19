87c2cf8f00ae5bf07a631fdbf22c3d95dfdcfef2
sarthak <sarthak.kaushik99@gmail.com>
2024-05-20 03:01:26 +0530

    CallBack Hell Part 1

--- Callbacks_Promises_Async.js
+++ Callbacks_Promises_Async.js
@@ -1,14 +1,52 @@
-console.log("Hello everyone!");
-function hello()
-{
-    // console.log("Hello I am Sarthak");
-}
+// console.log("Hello everyone!");
+// function hello()
+// {
+//     // console.log("Hello I am Sarthak");
+// }
 
-setTimeout(() => {
-    console.log("Hello I am Sarthak!");    // Timeout
-}, 3000);
+// setTimeout(() => {
+//     console.log("Hello I am Sarthak!");    // Timeout
+// }, 3000);
 
-console.log("Hello everyone again!");
+// console.log("Hello everyone again!");
 
 // This is called Asyncronous programming. The code is not executed in the order it is written.
 // The code is executed in the order of the time it takes to execute the code.
+
+
+function sum(a, b)
+{
+    console.log(a+b);
+}
+
+function sub(a, b)
+{
+    console.log(a-b);
+}
+
+function mul(a, b)
+{
+    console.log(a*b);
+
+}
+
+function div(a, b)
+{
+    console.log(a/b);
+
+}
+
+function calculator(a, b, sumcallback){
+    sumcallback(a, b);
+}
+
+calculator(5, 6, sum);
+calculator(5, 6, sub);
+calculator(5, 6, mul);
+calculator(5, 6, div);
+
+// This is callbacks, its use is that we can pass a function as an argument to another function.
+// This is used in asyncronous programming. We can pass multiple functions as arguments to another function.
+// An example is written above. We can pass multiple functions as arguments to the calculator function.
+
+

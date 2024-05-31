a7285274c05b33a9a967ac2a16463223cdfb7f17
sarthak <sarthak.kaushik99@gmail.com>
2024-06-01 01:48:16 +0530

    Small corrections are made in the current code

--- Callbacks_Promises_Async.js
+++ Callbacks_Promises_Async.js
@@ -14,36 +14,36 @@
 // The code is executed in the order of the time it takes to execute the code.
 
 
-function sum(a, b)
-{
-    console.log(a+b);
-}
+// function sum(a, b)
+// {
+//     console.log(a+b);
+// }
 
-function sub(a, b)
-{
-    console.log(a-b);
-}
+// function sub(a, b)
+// {
+//     console.log(a-b);
+// }
 
-function mul(a, b)
-{
-    console.log(a*b);
+// function mul(a, b)
+// {
+//     console.log(a*b);
 
-}
+// }
 
-function div(a, b)
-{
-    console.log(a/b);
+// function div(a, b)
+// {
+//     console.log(a/b);
 
-}
+// }
 
-function calculator(a, b, sumcallback){
-    sumcallback(a, b);
-}
+// function calculator(a, b, sumcallback){
+//     sumcallback(a, b);
+// }
 
-calculator(5, 6, sum);
-calculator(5, 6, sub);
-calculator(5, 6, mul);
-calculator(5, 6, div);
+// calculator(5, 6, sum);
+// calculator(5, 6, sub);
+// calculator(5, 6, mul);
+// calculator(5, 6, div);
 
 // This is callbacks, its use is that we can pass a function as an argument to another function.
 // This is used in asyncronous programming. We can pass multiple functions as arguments to another function.

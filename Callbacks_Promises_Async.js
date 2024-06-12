dd4a2e0eb2690329c8257bbb582977872a672728
sarthak <sarthak.kaushik99@gmail.com>
2024-06-12 10:17:48 +0530

    callback hell

--- Callbacks_Promises_Async.js
+++ Callbacks_Promises_Async.js
@@ -10,7 +10,7 @@
 
 // console.log("Hello everyone again!");
 
-// This is called Asyncronous programming. The code is not executed in the order it is written.
+                                               // This is called Asyncronous programming. The code is not executed in the order it is written.
 // The code is executed in the order of the time it takes to execute the code.
 
 
@@ -49,12 +49,44 @@
 // This is used in asyncronous programming. We can pass multiple functions as arguments to another function.
 // An example is written above. We can pass multiple functions as arguments to the calculator function.
 
-// nesting
 
-for (let i=0; i<5; i++){
-    let str = "";
-    for (let j = 0; j <5; j++){
-        str += j;
-    }
-    console.log(i, str);
+                                                          // nesting
+
+
+// for (let i=0; i<5; i++){
+//     let str = "";
+//     for (let j = 0; j <5; j++){
+//         str += j;
+//     }
+//     console.log(i, str);
+// }
+
+
+                                                          // Callback Hell
+
+
+
+// function getData(dataId){
+//     setTimeout(() => {
+//         console.log("data =", dataId);
+//     }, 2000);
+// }
+
+//getData(3, getData(2, getData(1)));  // Here the data will be shown instantly staqcked one after the other after the intial 2 sec interval.
+
+// Here the callback hell for executing the data sets after 2 secx interval each will be - 
+
+function getData(dataId, getNextData){
+    setTimeout(() => {
+        console.log("Data =", dataId);
+        getNextData();
+    }, 2000);
 }
+
+getData(10, () => {
+    getData(20, () => {
+        getData(30, () => {
+            getData(40);
+        });
+    });
+});

74c0f84fe31e1078c13af47cbd626494a0ca411e
sarthak <sarthak.kaushik99@gmail.com>
2024-06-13 01:55:02 +0530

    correcting spaces in between code

--- Callbacks_Promises_Async.js
+++ Callbacks_Promises_Async.js
@@ -10,7 +10,10 @@
 
 // console.log("Hello everyone again!");
 
+
                                                // This is called Asyncronous programming. The code is not executed in the order it is written.
+
+
 // The code is executed in the order of the time it takes to execute the code.
 
 

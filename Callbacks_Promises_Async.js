0d3c8a94c1d2c2738665ad9b60c71152a94d6405
sarthak <sarthak.kaushik99@gmail.com>
2024-06-13 03:33:22 +0530

    Promise and the code for explanation

--- Callbacks_Promises_Async.js
+++ Callbacks_Promises_Async.js
@@ -79,22 +79,42 @@
 
 // Here the callback hell for executing the data sets after 2 secx interval each will be - 
 
-function getData(dataId, getNextData){
-    setTimeout(() => {
-        console.log("Data =", dataId);
-        if(getNextData) {
-            getNextData();
-        }
-    }, 2000);
+// function getData(dataId, getNextData){
+//     setTimeout(() => {
+//         console.log("Data =", dataId);
+//         if(getNextData) {
+//             getNextData();
+//         }
+//     }, 2000);
+// }
+
+// getData(10, () => {
+//     console.log("Getting data set 2...");
+//     getData(20, () => {
+//         console.log("Getting data set 3...");
+//         getData(30, () => {
+//             console.log("Getting data set 4...");
+//             getData(40);
+//         });
+//     });
+// });
+
+
+                                                          // Promises
+
+
+function getData(dataId, getNextData){                  // getData function is created
+    return new Promise((resolve, reject) => {    // Creates a function PROMISE 
+        setTimeout(() => {
+            console.log("Data :", dataId);
+            resolve("Success");                         // If RESOLVE occurs
+            reject("Error has occured");                // If REJECT occurs
+            if(getNextData){
+                getNextData();
+            }
+        }, 5000);                                       // 5 Second Delay
+    });
 }
 
-getData(10, () => {
-    console.log("Getting data set 2...");
-    getData(20, () => {
-        console.log("Getting data set 3...");
-        getData(30, () => {
-            console.log("Getting data set 4...");
-            getData(40);
-        });
-    });
-});
+
+let result = new getData(10);                          // Creating an object "RESULT" and storing the value of getData() in it.

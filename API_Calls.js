0465a92222fd7fad4469ed9952e0e857380c0cfe
sarthak <sarthak.kaushik99@gmail.com>
2024-06-16 04:08:12 +0530

    FETCH API PART 3

--- API_Calls.js
+++ API_Calls.js
@@ -1,4 +1,6 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
+const btn = document.querySelector("#button1");
+const facts = document.querySelector("#para");
 
                                                     // Using Promise
 //let api = fetch(URL);
@@ -10,6 +12,10 @@ let getFacts = async () => {
     console.log("Fetching Data...");
     let response = await fetch(URL);
     console.log(response);                         // response is in JSON format
+    let data = await response.json();
+    facts.innerHTML = data[4].text;
 };
+
+addEventListener("click", getFacts);
 
 // getFacts();

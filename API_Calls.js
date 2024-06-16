1b57301b57ca0695ccfc03002f59f67e66761072
sarthak <sarthak.kaushik99@gmail.com>
2024-06-17 03:38:41 +0530

    FETCH API PART 4

--- API_Calls.js
+++ API_Calls.js
@@ -1,6 +1,6 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
-const btn = document.querySelector("#button1");
-const facts = document.querySelector("#para");
+// const URL = "https://cat-fact.herokuapp.com/facts";
+// const btn = document.querySelector("#button1");
+// const facts = document.querySelector("#para");
 
                                                     // Using Promise
 //let api = fetch(URL);
@@ -8,14 +8,28 @@ const facts = document.querySelector("#para");
 
                                                     // using Async - Await Function
 
-let getFacts = async () => {
-    console.log("Fetching Data...");
-    let response = await fetch(URL);
-    console.log(response);                         // response is in JSON format
-    let data = await response.json();
-    facts.innerHTML = data[4].text;
-};
+// let getFacts = async () => {
+//     console.log("Fetching Data...");
+//     let response = await fetch(URL);
+//     console.log(response);                         // response is in JSON format
+//     let data = await response.json();
+//     let x = Math.floor(Math.random() * 5);
+//     facts.innerHTML = data[x].text;                // data[4] is the 5th element in the JSON data
+// };                                                 // we used through the API.
 
-addEventListener("click", getFacts);
+
+// using Promise - Chaining Then & Catch method
+
+// function getFacts() { 
+//     fetch(URL)
+//     .then((response) => {
+//         return response.json();
+//     }).then((data) => {
+//         let x = Math.floor(Math.random() * 5);
+//         facts.innerHTML = data[x].text;
+//     })
+// }
+
+// addEventListener("click", getFacts);
 
 // getFacts();
